2608c83f36b66967f7b659b6f96a256ffc0adcf2
yule57 <cdyule@jd.com>
2019-03-06 18:34:57 +0800

    chore: some commitlint changes

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,17 @@
+/**
+ * *docs:
+ * *https://github.com/conventional-changelog/commitlint/blob/master/docs/reference-rules.md
+ */
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'type-enum': [2, 'always', [
-            "feat", "fix", "docs", "style", "refactor", "perf", "test", "build", "ci", "chore", "revert"
-        ]],
-        'subject-full-stop': [0, 'never'],
-        'subject-case': [0, 'never']
+        'type-enum': [
+            2,
+            'always',
+            ["feat", "fix", "docs", "style", "refactor", "perf", "test", "build", "ci", "chore", "revert"]
+        ],
+        'subject-case': [0, 'never'],
+        'subject-max-length': [2, 'always', 50],
+        'scope-case': [0, 'never']
     }
 };
